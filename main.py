e7dc3bf2a46122f751e214a843341b18cb0a7641
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-04-07 10:43:23 -0400

    UPDATE

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, abort, redirect, url_for
 from markupsafe import escape
+from werkzeug.utils import secure_filename
+
 
 
 app = Flask(__name__)
@@ -70,3 +72,103 @@ def login_post():
 @app.route('/hello/<name>')
 def hello_name(name=None):
     return render_template('hello.html', name=name)
+
+
+#To access parameters submitted in the URL (?key=value) you can use the args attribute:
+
+#searchword = request.args.get('key', '')
+@app.route('/login_access', methods=['POST', 'GET'])
+def login_access():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] and request.form['password']:
+            return f'Nombre: {request.form["username"]}'
+
+        else:
+            error = 'Invalid username/password'
+    else:
+            error = 'Metodo no valido'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
+
+
+
+
+@app.route('/upload_secure', methods=['GET', 'POST'])
+def upload_file_secure():
+    if request.method == 'POST':
+        file = request.files['the_file']
+        file.save(f"/var/www/uploads/{secure_filename(file.filename)}")
+
+
+
+
+@app.route('/index_error')
+def index_error():
+    return redirect(url_for('login'))
+
+@app.route('/login_error')
+def login_error():
+    abort(401)
+    #this_is_never_executed()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
+
+# API 
+# @app.route("/me")
+# def me_api():
+#     user = get_current_user()
+#     return {
+#         "username": user.username,
+#         "theme": user.theme,
+#         "image": url_for("user_image", filename=user.image),
+#     }
+
+# @app.route("/users")
+# def users_api():
+#     users = get_all_users()
+#     return [user.to_json() for user in users]
+
+
+#SESSIONS
+
+#from flask import session
+
+# Set the secret key to some random bytes. Keep this really secret!
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# @app.route('/')
+# def index():
+#     if 'username' in session:
+#         return f'Logged in as {session["username"]}'
+#     return 'You are not logged in'
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         session['username'] = request.form['username']
+#         return redirect(url_for('index'))
+#     return '''
+#         <form method="post">
+#             <p><input type=text name=username>
+#             <p><input type=submit value=Login>
+#         </form>
+#     '''
+
+# @app.route('/logout')
+# def logout():
+#     # remove the username from the session if it's there
+#     session.pop('username', None)
+#     return redirect(url_for('index'))
--- readme.md
+++ readme.md
@@ -5,12 +5,19 @@ https://flask.palletsprojects.com/en/3.0.x/quickstart/
 ```
 
 ```
-pip install flask
+pip install flask gunicorn
 ```
 ```
 python3 -m venv .venv
 
 ```
+## Run on develop
 ```
 flask --app main run --host=0.0.0.0 --debug
+```
+
+
+## run on production
+```
+gunicorn -w 4 -b 0.0.0.0:5000 main:app
 ```
